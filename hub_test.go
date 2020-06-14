1bc27279174f093e07760642e2742bd4b7d73738
Nik Silver <nik@niksilver.com>
2020-06-14 11:10:34 +0100

    Sped up TestHub_JoinerMessagesHappen.

--- hub_test.go
+++ hub_test.go
@@ -532,6 +532,16 @@ func TestHub_GeneralChaos(t *testing.T) {
 }
 
 func TestHub_JoinerMessagesHappen(t *testing.T) {
+	// Just for this test, lower the reconnectionTimeout so that a
+	// Leaver message is triggered reasonably quickly.
+
+	oldReconnectionTimeout := reconnectionTimeout
+	reconnectionTimeout = 250 * time.Millisecond
+	defer func() {
+		reconnectionTimeout = oldReconnectionTimeout
+	}()
+
+	// Start a server
 	serv := newTestServer(bounceHandler)
 	defer serv.Close()
 
