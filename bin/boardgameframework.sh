149f9f71d06f3abbd9063d67127b4ff70d27a656
Nik Silver <nik@niksilver.com>
2020-08-07 15:46:37 +0100

    Attempt to use ssl-proxy for the server.

--- bin/boardgameframework.sh
+++ bin/boardgameframework.sh
@@ -7,5 +7,10 @@ LOGFILENAME=boardgameframework.$DAY.log
 mkdir -p $LOGDIR
 
 THISDIR=`dirname "$0"`
-export PORT=80
-exec "$THISDIR/boardgameframework" >$LOGDIR/$LOGFILENAME
+
+# SSL proxy from https://github.com/suyashkumar/ssl-proxy/releases/
+"$THISDIR/ssl-proxy" -from 0.0.0.0:443 -to 127.0.0.1:8080 -domain=bgf.pigsaw.org >>$LOGDIR/$LOGFILENAME &
+
+# Start the board game framework server
+export PORT=8080
+exec "$THISDIR/boardgameframework" >>$LOGDIR/$LOGFILENAME
