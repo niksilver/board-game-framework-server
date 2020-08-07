878012a052d111d289f2323a1b95e633be7312e8
Nik Silver <nik@niksilver.com>
2020-08-07 17:06:44 +0100

    ssl-proxy started as its own service.

--- bin/boardgameframework.sh
+++ bin/boardgameframework.sh
@@ -8,9 +8,6 @@ mkdir -p $LOGDIR
 
 THISDIR=`dirname "$0"`
 
-# SSL proxy from https://github.com/suyashkumar/ssl-proxy/releases/
-"$THISDIR/ssl-proxy" -from 0.0.0.0:443 -to 127.0.0.1:8080 -domain=bgf.pigsaw.org >>$LOGDIR/$LOGFILENAME &
-
 # Start the board game framework server
 export PORT=8080
 exec "$THISDIR/boardgameframework" >>$LOGDIR/$LOGFILENAME
---
+++ bin/ssl-proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DAY=`date +"%a"`
+LOGDIR=/var/log/ssl-proxy
+LOGFILENAME=ssl-proxy.$DAY.log
+
+mkdir -p $LOGDIR
+
+THISDIR=`dirname "$0"`
+
+# SSL proxy from https://github.com/suyashkumar/ssl-proxy/releases/
+exec "$THISDIR/ssl-proxy" -from 0.0.0.0:443 -to 127.0.0.1:8080 -domain=bgf.pigsaw.org >>$LOGDIR/$LOGFILENAME
